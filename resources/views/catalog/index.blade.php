4ad5d65167b35423d1008e390ae83c93cda02e1b
Spadovv <113766331+Spadovv@users.noreply.github.com>
2022-12-09 21:40:07 +0400

    Update index.blade.php

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Каталог товаров</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ducimus, eligendi exercitationem expedita, iure iusto laborum magnam qui quidem repellat similique tempora tempore ullam! Deserunt doloremque impedit quis repudiandae voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ducimus, eligendi exercitationem expedita, iure iusto laborum magnam qui quidem repellat similique tempora tempore ullam! Deserunt doloremque impedit quis repudiandae voluptas.</p>
+    <p>Для клиента очень важно обратить внимание на процесс адиписцирования. И мы ведем, с целесообразным осуществлением выбора, с правом и справедливым правом большого труда, который действительно отталкивает подобные времена в любое время! Они уходят, и боль мешает получению удовольствия от развода. Для клиента очень важно обратить внимание на процесс адиписцирования. И мы ведем, с целесообразным осуществлением выбора, с правом и справедливым правом большого труда, который действительно отталкивает подобные времена в любое время! Они уходят, и боль мешает получению удовольствия от развода.</p>
 
     <h2 class="mb-4">Разделы каталога</h2>
     <div class="row">
